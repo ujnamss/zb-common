6124198227cd83e19c638a02586b70def17e7805
Manjunath Somashekar <ujnamss@gmail.com>
2018-11-04 00:36:46 -0700

    added functions to get time since epoch

--- zb-common/common.py
+++ zb-common/common.py
@@ -9,3 +9,9 @@ class Util:
 
     def get_env_value(self, key, section):
         return os.getenv("{}_{}".format(section, key), self.conf.get(section, key))
+
+    def getSecondsSinceEpoch(self):
+        return int(time.time())
+
+    def getMillisecondsSinceEpoch(self):
+        return int(1000 * self.getSecondsSinceEpoch())
